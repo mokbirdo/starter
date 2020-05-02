c31702a60999b4f12f6a8e2f499d6aa789dd53cc
Дмитрий Мичкире <michkire@gmail.com>
2020-05-03 02:06:42 +0300

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,30 +1,17 @@
-<?php
-
-namespace App\Http\Controllers;
-
+<?php namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-class TestController extends Controller
-{
-    public function i_am_test(Request $request)
-    {
+class TestController extends Controller{public function i_am_test(Request $request)    {
         $var = 1;
         $VAR = 2;
         $snake_var = 2;
         $camelCaseVar = 3;
         $UcFirstCamelCaseVar = 4;
 
-        return [$var, $VAR, $snake_var, $camelCaseVar, $UcFirstCamelCaseVar];
-    }
+        return [$var, $VAR, $snake_var, $camelCaseVar, $UcFirstCamelCaseVar]; }
 
-    public function iAmTest(Request $request)
-    {
+    public function iAmTest(Request $request)    {
         $var = 1;
         $VAR = 2;
-        $snake_var = 2;
-        $camelCaseVar = 3;
-        $UcFirstCamelCaseVar = 4;
-
-        return [$var, $VAR, $snake_var, $camelCaseVar, $UcFirstCamelCaseVar];
-    }
+        $snake_var = 2;$camelCaseVar = 3;$UcFirstCamelCaseVar = 4;return [$var, $VAR, $snake_var, $camelCaseVar, $UcFirstCamelCaseVar]; }
 }
