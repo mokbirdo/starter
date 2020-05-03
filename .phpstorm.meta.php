9931879ffeaefe7fd422208267ccd2ab307968a3
Дмитрий Мичкире <michkire@gmail.com>
2020-05-03 07:45:45 +0300

    no message

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-03 04:43:59.
+    * Generated on 2020-05-03 04:45:14.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
