68969e71f3d7aa9cf73d82eecc6282ad9bcd3296
Дмитрий Мичкире <michkire@gmail.com>
2020-05-03 06:00:41 +0300

    no message

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.9.2 on 2020-05-03 02:56:18.
+ * Generated for Laravel 7.9.2 on 2020-05-03 02:58:44.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
