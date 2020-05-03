adce06e74fa190941eb39d67fa3660f7f7903276
Дмитрий Мичкире <michkire@gmail.com>
2020-05-03 07:51:41 +0300

    fix QWERT-555 test comments

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.9.2 on 2020-05-03 04:45:14.
+ * Generated for Laravel 7.9.2 on 2020-05-03 04:50:48.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
